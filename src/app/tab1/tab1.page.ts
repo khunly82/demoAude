0baf59416b85a08973f085655188b36570ba138c
khun <lykhun@gmail.com>
2023-05-31 21:48:07 +0200

    initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
+import { ActionSheetController, IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, FormsModule, CommonModule],
 })
 export class Tab1Page {
-  constructor() {}
+
+  articleName: string|null = null;
+  articles: { name: string, checked: boolean }[] = []
+
+  constructor(
+    private readonly actionSheetCtrl: ActionSheetController
+  ) {}
+
+  add() {
+    if(!this.articleName)
+      return;
+    this.articles = [ ...this.articles, { name: this.articleName, checked: false }];
+    this.articleName = null;
+  }
+
+  async openActionSheet(article: {name: string, checked: boolean}) {
+    const as = await this.actionSheetCtrl.create({
+      header: 'actions',
+      buttons: [{
+        text: article.checked ? 'Uncheck' : 'Check',
+        icon: article.checked ? 'remove' : 'checkmark',
+        handler: () => {
+          article.checked = !article.checked
+        }
+      }, {
+        text: 'Delete',
+        icon: 'trash',
+        role: 'destructive',
+        handler: () => {
+          this.articles = this.articles.filter(a => a !== article);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel'
+      }]
+    });
+
+    as.present();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BarcodeScanner, SupportedFormat } from '@capacitor-community/barcode-scanner';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent]
+  imports: [IonicModule, CommonModule]
 })
 export class Tab2Page {
 
+  result : string|undefined = undefined;
+  scanActive: boolean = false;
+
   constructor() {}
+
+  async ionViewDidEnter() {
+    const result = await BarcodeScanner.checkPermission({ force: true });
+    if(!result) {
+      // afficher message
+    }
+  }
+
+  async scan() {
+    await BarcodeScanner.prepare();
+    this.scanActive = true;
+    document.body.style.background = 'transparent'
+    const scanResult = await BarcodeScanner.startScan({ targetedFormats: [SupportedFormat.EAN_13] });
+    this.stopScan();
+
+    this.result = scanResult.content;
+  }
+
+  stopScan() {
+    this.scanActive = false;
+    document.body.style.background = 'unset'
+  }
 
 }
